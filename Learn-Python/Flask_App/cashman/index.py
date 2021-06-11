22637962b8d1cfa6519ed8126a9e232f8f1cb072
Tidbits JS <tidbitsjs@gmail.com>
2021-06-11 22:04:42 +0530

    income & expense routes

--- Learn-Python/Flask_App/cashman/index.py
+++ Learn-Python/Flask_App/cashman/index.py
@@ -1,23 +1,51 @@
 from flask import Flask, jsonify, request
+from marshmallow import schema
+
+from .models.expense import Expense, ExpenseSchema
+from .models.income import Income, IncomeSchema
+from .models.transaction_type import TransactionType
 
 app = Flask(__name__)
 
-incomes = [
-    {'description': 'salary', 'amount': 5000}
+transactions = [
+    Income('Salary', 5000),
+    Income('Dividends', 200),
+    Expense('pizza', 50),
+    Expense('Rock Concert', 100)
 ]
 
 
-@app.route("/")
-def welcome():
-    return "Welcome to Cashman - A Flask Restful API App"
-
-
-@app.route("/incomes")
+@app.route('/incomes')
 def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
     return jsonify(incomes)
 
 
-@app.route("/incomes", methods=['POST'])
+@app.route('/incomes', methods=['POST'])
 def add_income():
-    incomes.append(request.get_json())
-    return 'Item Added!', 204
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income)
+    return "Income Added!", 204
+
+
+@app.route('/expenses')
+def get_expenses():
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+    )
+    return jsonify(expenses)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
+    return "Expense Added!", 204
+
+
+if __name__ == "__main__":
+    app.run()
